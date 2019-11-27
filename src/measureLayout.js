99221e43d9aefa6bc78eb2b894c87c06d2d221bb
jamie-birch <jamie.birch@s-and-t.com>
2019-11-27 19:12:57 +0000

    Still doesn't fix native due to layout occurring later than onBecameFocused fires, but worth keeping a record of.

--- src/measureLayout.js
+++ src/measureLayout.js
@@ -24,13 +24,42 @@ const getRect = (node) => {
 const measureLayout = (node, callback) => {
   const relativeNode = node && node.parentNode;
 
-  if (node && relativeNode) {
+  if(!node){
+     return;
+  }
+
+  if (relativeNode) {
+    /* i.e. is a Web DOM node. */
     const relativeRect = getRect(relativeNode);
     const {height, left, top, width} = getRect(node);
     const x = left - relativeRect.left;
     const y = top - relativeRect.top;
 
     callback(x, y, width, height, left, top);
+  } else {
+    /* i.e. is a React Native node.
+     * https://facebook.github.io/react-native/docs/direct-manipulation#other-native-methods */
+    node.measure((fx, fy, width, height, px, py) => {
+      var frameRect = {
+        width: width,
+        height: height,
+        left: fx,
+        top: fy,
+      };
+      var pageRect = {
+        width: width,
+        height: height,
+        left: px,
+        top: py,
+      };
+      
+      /* Complete guess; probably wrong, as it's unlikely to be equivalent to the available DOM properties. */
+      var x = pageRect.left - frameRect.left;
+      var y = pageRect.top - frameRect.top;
+
+      callback(x, y, frameRect.width, frameRect.height, frameRect.left, frameRect.top);
+    });
+
   }
 };
 
